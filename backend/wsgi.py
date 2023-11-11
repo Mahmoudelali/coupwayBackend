ec19bf388c59c5feb0b7288d957366d08f32018c
Mahmoud_elali <mahmud.elali98@gmail.com>
2023-11-11 22:24:50 +0200

    deploy settings

---
+++ backend/wsgi.py
@@ -0,0 +1,24 @@
+"""
+WSGI config for backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
+application = get_wsgi_application()
+
+
+module_name = application.__module__
+
+# Extract the app name
+app_name = module_name.split('.')[0]
+
+print("Django App Name:", app_name)
